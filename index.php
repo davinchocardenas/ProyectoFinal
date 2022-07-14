35f96f244b976819c113a1e842e6f8ba653b13c7
unknown <emunoz5498@misena.edu.co>
2022-07-14 16:54:38 -0500

    nuevo index

--- index.php
+++ index.php
@@ -1,35 +1,47 @@
 <?php
 
-    /////////////////////////////////////////////
-    //                 CONTROLLER              //
-    /////////////////////////////////////////////
-    require_once 'controller/user.controller.php';
-    require_once 'controller/template.controller.php';
-    require_once 'controller/proveedor.controller.php';
+    //////CONTROLLER//////
+    require_once "controller/categoria.controller.php";
+    require_once "controller/bodega.controller.php";
+    require_once "controller/proveedor.controller.php";
+    require_once "controller/usuario.controller.php";
+    require_once "controller/template.controller.php";
+    require_once "controller/producto.controller.php";
+    require_once "controller/entrada.controller.php";
+    ////MODELS///////
 
-    /////////////////////////////////////////////
-    //                   MODEL                 //
-    /////////////////////////////////////////////
-    require_once 'model/dao/user.dao.php';
-    require_once 'model/dto/user.dto.php';
-    require_once 'model/dao/proveedor.dao.php';
-    require_once 'model/dto/proveedor.dto.php';
+    require_once "model/dao/usuario.dao.php";
+    require_once "model/dto/usuario.dto.php";
+
+    require_once "model/dao/proveedor.dao.php";
+    require_once "model/dto/proveedor.dto.php";
+
+    require_once "model/dao/bodega.dao.php";
+    require_once "model/dto/bodega.dto.php";
+
+    require_once "model/dao/categoria.dao.php";
+    require_once "model/dto/categoria.dto.php";
+
+    require_once "model/dao/producto.dao.php";
+    require_once "model/dto/producto.dto.php";
+
+    require_once "model/dao/entrada.dao.php";
+    require_once "model/dto/entrada.dto.php";
+
+
+    ////// CONEXION /////
+
+    require_once "model/conexion.php";
+
+
+    ////////LIBRERIAS//////////////
     
-    /////////////////////////////////////////////
-    //                 CONEXION                //
-    /////////////////////////////////////////////
-    require_once 'model/conexion.php';
+   
+
+    /// start///
+
+    $objrun = new template();
+    $objrun -> getIntro();
 
 
-        /////////////////////////////////////////////
-    //                 LIBRARYS                //
-    /////////////////////////////////////////////
-    
-
-
-    /////////////////////////////////////////////
-                    /* run */
-    $objRun = new Template();
-    $objRun->getIntro();
-    
 ?>
